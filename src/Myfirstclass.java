de16aedc5303c815241420f87a128cdf76465586
Srishti Goel <Srishti.Goel@NIIT-Tech.com>
2019-12-05 15:10:25 +0530

    First Shristi

---
+++ src/Myfirstclass.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Myfirstclass {
+
+	@Test
+	public void z()
+	{
+		System.out.println("execute lase ");
+	}
+	@Test
+	public void getData()
+	{
+		System.out.println("abcd");
+	}
+	
+	
+	@Test
+	public void aetData()
+	{
+		System.out.println("efgh");
+	}
+	
+	@Test
+	public void assertion()
+	{
+		assertEquals("abcdefgh", "hellp","hellp");
+	
+		
+			System.out.println(" lase ");
+		
+	}
+	
+}
---
+++ src/Secondlife.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+public class Secondlife {
+
+	
+	
+	@Test
+	public void new1()
+	{
+		System.out.println("new 1");
+		
+		
+	}
+	
+	@Test
+	public void aaa()
+	{
+		System.out.println("new 12");
+		
+		
+	}
+	@Test
+	public void bnew1()
+	{
+		System.out.println("new 1w");
+		
+		
+	}
+}
